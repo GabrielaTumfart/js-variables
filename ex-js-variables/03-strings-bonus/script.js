03f0986a841f8479607c36b718726c378e4bf4cd
GabrielaTumfart <gabrielatumfart@gmail.com>
2025-12-15 17:31:24 +0100

    Correzione esercicio 1

--- ex-js-variables/03-strings-bonus/script.js
+++ ex-js-variables/03-strings-bonus/script.js
@@ -6,17 +6,17 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 4;
+const cityIndex = city.;
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = 'York';
+const citySubstring = city.;
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = 'New Delhi';
+const cityReplaced = city.;
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
@@ -27,5 +27,5 @@ console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
-const upperCaseHello = 'HELLO';
+const upperCaseHello = hello.;
 console.log(upperCaseHello); // Output: "HELLO"
